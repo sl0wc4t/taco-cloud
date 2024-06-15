d3c130a7a4957a63e9bb9eb1bf6b4e81740c145a
sl0wc4t <sl0wc4t@gmail.com>
2024-06-15 14:30:54 +0500

    taco-cloud/Feature 3.3.4

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -3,6 +3,13 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.TacoOrder;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+
+    List<TacoOrder> findByDeliveryZip(String deliveryZip);
+
+    List<TacoOrder> findByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 
 }
