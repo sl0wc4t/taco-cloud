81ae14802454ef30dd5293f17bb12aefb414e244
sl0wc4t <sl0wc4t@gmail.com>
2024-06-16 13:15:58 +0500

    taco-cloud/Feature 7.1.2

--- src/main/java/tacos/web/api/TacoController.java
+++ src/main/java/tacos/web/api/TacoController.java
@@ -33,4 +33,10 @@ public class TacoController {
         return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco) {
+        return tacoRepo.save(taco);
+    }
+
 }
